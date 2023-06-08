1b7ca8efe4e2d36eb9fd3e5251790990852d0076
JackWilliamVass <jackjackv05@gmail.com>
2023-06-08 15:04:30 +0100

    never done
    !!!!!!!!!!! done never

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var what = "WHAT/?? HOW IS HE DOING THI?!?!?!? I'VE NEVER DONE C# BEFORE";
+Console.WriteLine(what);
+Console.WriteLine("Hello, World!");
